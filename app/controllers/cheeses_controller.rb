3f95d11de1ee822b051e9bf9c06e3bf9d842157c
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-09-30 13:01:16 +0300

    commit tree c08a6a5068b3e71dda15f8720708b60a846eb22a

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -7,9 +7,42 @@ class CheesesController < ApplicationController
   end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese
+  # end
+# def show
+#   cheese = Cheese.find_by(id: params[:id])
+#   render json: {
+#     id: cheese.id,
+#     name: cheese.name,
+#     price: cheese.price,
+#     is_best_seller: cheese.is_best_seller
+#   }
+# end
 
+# def index
+#   cheeses = Cheese.all
+#   render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+# end
+
+# def index
+#   cheeses = Cheese.all
+#   render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+# end
+
+# def show
+#   cheese = Cheese.find_by(id: params[:id])
+#   render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+# end
+
+def show
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    # status: :not_found will produce a 404 status code
+    render json: { error: 'Cheese not found' }, status: :not_found
+  end
+end
 end
